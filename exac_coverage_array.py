5eb3d626bbd45e129643ad1263c90584c2476e94
stuartcannon <stuartcannon@nhs.net>
2017-06-15 16:58:47 +0100

    returns individuals at 20x for coordinate

---
+++ exac_coverage_array.py
@@ -0,0 +1,23 @@
+import sys, requests
+
+def exac_coverage_array(chrom, coord):
+    server = "http://exac.hms.harvard.edu/"
+    headers = {"Content-Type" : "application/json"}
+    coord_plus = int(coord) + 1
+    ext = "rest/region/coverage_array/" + chrom + "-" + coord + "-" + str(coord_plus)
+    print(server + ext)
+    r = requests.get(server + ext, headers=headers)
+    r_json = r.json()
+    for item in r_json:
+        if str(item['pos']) == (coord):
+            if item['has_coverage'] == False:
+                print(0)
+                return '0'
+            else:
+                x = int(float(item['20'])*60706)
+                print(x)
+                return str(x)
+        else:
+            pass
+            
+x = exac_coverage_array(sys.argv[1], sys.argv[2])
